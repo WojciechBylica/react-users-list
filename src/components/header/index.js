056023fd8db5159bd54ab4dffa5078fadff62680
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-04 00:48:46 +0200

    added logo image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,10 @@ import './style.css';
 const Header = () => (
   <header className="header">
     <div className="header__box">
-      <h1 className="header__title">Users List</h1>
+      <h1 className="header__title">
+        <img width={50} height={50} alt="logo" src="/logo.png" />
+        Users List
+      </h1>
       <Nav />
     </div>
   </header>
