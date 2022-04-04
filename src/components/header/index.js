860c9d666b80eae503549e231e38ad7b8a1a39cc
Wojciech Bylica <wojtekbyl@gmail.com>
2022-04-04 11:08:00 +0200

    image src changed

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,12 @@
 import { Nav } from '../index';
 import './style.css';
+import image from './logo.png';
 
 const Header = () => (
   <header className="header">
     <div className="header__box">
       <h1 className="header__title">
-        <img width={50} height={50} alt="logo" src="/logo.png" />
+        <img width={50} height={50} alt="logo" src={image} />
         Users List
       </h1>
       <Nav />
